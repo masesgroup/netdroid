5021883d1cb721989aead060e4b038c11803164c
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2024-06-03 05:46:46 +0200

    Removed unused namespace creating conflict (#33)

--- src/net/Netdroid/Generated/Java/Lang/AllPackageClasses.cs
+++ src/net/Netdroid/Generated/Java/Lang/AllPackageClasses.cs
@@ -1836,49 +1836,6 @@ namespace Java.Lang
     }
     #endregion
 
-    #region Runtime
-    /// <summary>
-    /// <see href="https://developer.android.com/reference/java/lang/Runtime.html"/>
-    /// </summary>
-    public partial class Runtime : MASES.JCOBridge.C2JBridge.JVMBridgeBase<Runtime>
-    {
-        const string _bridgeClassName = "java.lang.Runtime";
-        /// <summary>
-        /// Default constructor: even if the corresponding Java class does not have one, it is mandatory for JCOBridge
-        /// </summary>
-        public Runtime() { }
-        /// <summary>
-        /// Generic constructor: it is useful for JCOBridge when there is a derived class which needs to pass arguments to the highest JVMBridgeBase class
-        /// </summary>
-        public Runtime(params object[] args) : base(args) { }
-
-        private static readonly IJavaType LocalBridgeClazz = ClazzOf(_bridgeClassName);
-
-        /// <summary>
-        /// <see href="https://www.jcobridge.com/api-clr/html/P_MASES_JCOBridge_C2JBridge_JVMBridgeBase_BridgeClassName.htm"/>
-        /// </summary>
-        public override string BridgeClassName => _bridgeClassName;
-        /// <summary>
-        /// <see href="https://www.jcobridge.com/api-clr/html/P_MASES_JCOBridge_C2JBridge_JVMBridgeBase_IsBridgeAbstract.htm"/>
-        /// </summary>
-        public override bool IsBridgeAbstract => false;
-        /// <summary>
-        /// <see href="https://www.jcobridge.com/api-clr/html/P_MASES_JCOBridge_C2JBridge_JVMBridgeBase_IsBridgeCloseable.htm"/>
-        /// </summary>
-        public override bool IsBridgeCloseable => false;
-        /// <summary>
-        /// <see href="https://www.jcobridge.com/api-clr/html/P_MASES_JCOBridge_C2JBridge_JVMBridgeBase_IsBridgeInterface.htm"/>
-        /// </summary>
-        public override bool IsBridgeInterface => false;
-        /// <summary>
-        /// <see href="https://www.jcobridge.com/api-clr/html/P_MASES_JCOBridge_C2JBridge_JVMBridgeBase_IsBridgeStatic.htm"/>
-        /// </summary>
-        public override bool IsBridgeStatic => false;
-
-        // TODO: complete the class
-
-    }
-    #endregion
 
     #region RuntimePermission
     /// <summary>
